2f55b4287403a5f4a1209945e175163569b9e853
CodeZaru <teamsullivank@gmail.com>
2017-11-09 02:03:05 -0500

    update server file

--- server.js
+++ server.js
@@ -22,10 +22,27 @@ var PORT = process.env.PORT || 3000;
 // ==============================================================================
 // INSTRUCT THE SERVER "app" TO USE VARIOUS RESOURCES LIKE MIDDLEWARE AND
 // SERVER API ROUTE DEFINITIONS 
+
 // Note: If you don’t have the bodyParser middleware the server won’t 
 // understand your POST…,  that is very important two lines b/c the 
 // method of POST sends to server, need server to understand.
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));//true/false kakunin shinai to..
 app.use(bodyParser.json());
+app.use(bodyParser.text());
+// koko ni..
+app.use(express.static('app'));
+
+// ROUTES
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function(){
+	console.log("App listening on PORT: " + PORT);
+});
