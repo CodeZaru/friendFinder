d5b9d6d6f9e6b00b401b751123bb43165275846f
CodeZaru <teamsullivank@gmail.com>
2017-11-11 02:30:21 -0500

    updated files

---
+++ app/data/friendsMW.js
@@ -0,0 +1,18 @@
+//Friend list information; Friends API
+
+var friends = [ {
+	"name" : "F1",
+	"photo" : "https://media1.popsugar-assets.com/files/thumbor/wW6339B_N7MugJmadGRXTVYg5vY/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2016/09/06/024/n/1922398/be4b94fdd833aadf_GettyImages-53240674/i/Pon-de-Replay-Rihanna.jpg",
+	"scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+	"name" : "F2",
+	"photo" : "http://www.billboard.com/files/media/shakira-portrait-sesssion-2015-billboard-1548.jpg",
+	"scores" : [3,3,3,3,3,3,3,3,3,3]
+}, {
+	"name" : "F3",
+	"photo" : "https://scdailymakeover.files.wordpress.com/2015/07/news-sofia-vergara.jpg?w=610&h=773",
+	"scores" : [5,5,5,5,5,5,5,5,5,5]
+}];
+
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,63 @@
+// Includes two routes:
+// GET route to "/api/friends" to get from friends.js 
+
+// Grab the saved data from friends array of friend profile objects
+var friendArr = require('../data/friendsMW.js');
+
+//ks
+//var profiles=[];
+
+//Use express() to access friends api
+module.exports = function (app) {
+	//Using the GET method to grab res data from friends.js holding friends api
+	app.get('/api/friendsMW', function(req, res){
+		res.json(friendArr);
+	})
+
+	//Using the POST method to add user answers/choices into friends api
+	app.post('/api/friendsMW', function(req, res){
+
+		var user = req.body;
+
+		for(var i = 0; i < user.scores.length; i++) {
+			if(user.scores[i] == "1 (Strongly Disagree)") {
+				user.scores[i] = 1; //defines 1 as an int
+			} else if(user.scores[i] == "5 (Strongly Agree)") {
+				user.scores[i] = 5; //defines 5 as an int
+			} else {
+				user.scores[i] = parseInt(user.scores[i]); //change 2,3, or 4 to an int
+			}
+		}
+
+		var diffArr = []; //this will be the new .score for the user object in api
+
+		for(var i = 0; i < friendArr.length; i++) {
+			
+			var diffTotal = 0;
+			
+			for(var j = 0; j < friendArr[i].scores.length; j++) {
+				var diffScore = Math.abs(friendArr[i].scores[j] - user.scores[j]);
+				diffTotal += diffScore;
+			}
+			diffArr[i] = diffTotal;
+		}
+
+		var friendNumber = diffArr[0];
+		var friendIndex = 0;
+
+		for(var i = 1; i < diffArr.length; i++) {
+			if(diffArr[i] < friendNumber) {
+				friendNumber = diffArr[i];
+				friendIndex = i;
+			}
+		}
+
+		friendArr.push(user);
+
+
+		res.json(friendArr[friendIndex]);
+	})//end POST
+}
+
+
+
